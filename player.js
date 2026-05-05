b404b8668c9030f0e8e0415309ee8aa691127aed
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-05-05 22:32:12 +0300

    Delete player.js

--- player.js
+++
@@ -1,9 +0,0 @@
-class Player{
-  constructor(x, y, width, heigth){
-    this.x: x;
-    this.y: y;
-    this.width = window.DATA.PLAYER.width;
-    this.heigth = window.DATA.PLAYER.heigth
-  }
-}
-window.Player = new Player();
